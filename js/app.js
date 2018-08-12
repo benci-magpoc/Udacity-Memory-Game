ba498f26958e6d4746e38f9144a098a52171889e
sangumee <super2451894@gmail.com>
2018-08-12 18:40:05 +0900

    shuffle the card and print
    Randomly shuffled card

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 /*
  * Create a list that holds all of your cards
  */
-
+let cardList = Array.from(document.getElementsByClassName('card'));
+console.log(cardList);
 
 /*
  * Display the cards on the page
@@ -12,7 +13,8 @@
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -25,6 +27,17 @@ function shuffle(array) {
     return array;
 }
 
+cardList = shuffle(cardList);
+console.log(cardList);
+
+const removeElements = (elms) => [...elms].forEach(el => el.remove());
+removeElements(document.querySelectorAll(".card"));
+
+
+const list = document.querySelector('.deck');
+for (let i = 0; i < cardList.length; i++) {
+    list.appendChild(cardList[i]);
+}
 
 /*
  * set up the event listener for a card. If a card is clicked:
