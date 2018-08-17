732c3e65d6f6719cf6cc17f116f41706e2ffbb7e
sangumee <super2451894@gmail.com>
2018-08-17 21:35:19 +0900

    import VEX
    VEX

--- js/app.js
+++ js/app.js
@@ -2,14 +2,7 @@
  * Create a list that holds all of your cards
  */
 let cardList = Array.from(document.getElementsByClassName('card'));
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
-// Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length,
         temporaryValue, randomIndex;
@@ -39,46 +32,7 @@ for (const cardLists of cardList) {
 const restart = document.querySelector('.restart');
 restart.addEventListener('click', function () {
     window.location.reload();
-})
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
-// card match - card open show
-// $(document).ready(function () {
-//     $('.card').click(function () {
-//         // Card Open
-//         $(this).addClass("open show");
-
-//         // Save First Click i element className
-//         let firstclick = ($(this).children('.fa'));
-//         console.log(firstclick);
-//         // Save Second Click i element className
-//         let secondclick = ($(this).children('.fa'));
-//         console.log(secondclick);
-
-//         // compare first click element and second element to match card
-//         if (firstclick === secondclick) {
-//             // if match each other change card class to "card match"
-//             $(this).removeClass("open show");
-//             $(this).addClass("match")
-//         } else {
-//             // if not match each other close card after 2 seconds.
-//             let ele = $(this);
-//             setTimeout(function () {
-//                 ele.removeClass("open show");
-//             }, 2000);
-//         }
-//     })
-// });
+});
 
 function initStars() {
     for (let i = 0; i < 5; i++) {
@@ -92,6 +46,21 @@ function reduceStar() {
     $(stars[stars.length - 1]).toggleClass("fa-star fa-star-o");
 }
 
+// Timer Settings
+let started = false;
+let timeCount = 0;
+let timerPrint;
+let popup ='<div id="ex1" class="modal"><p>Thanks for clicking. That felt good.</p><a href="#" rel="modal:close">Close</a></div>';
+// starts the timer
+function startTimer() {
+    timeCount += 1;
+    $(".timer").html(timeCount);
+    timerPrint = setTimeout(startTimer, 1000);
+}
+
+// Check Match count
+let matchCount = 0;
+
 // Create initial Stars
 initStars();
 
@@ -100,7 +69,7 @@ let movesCount = -1;
 function moves() {
     movesCount += 1;
     $('.moves').html(movesCount);
-    if (movesCount === 14 || movesCount === 20 || movesCount === 25 || movesCount === 30 || movesCount === 34) {
+    if (movesCount === 12 || movesCount === 16 || movesCount === 20 || movesCount === 24 || movesCount === 28) {
         reduceStar();
     }
     if (movesCount === 34) {
@@ -109,30 +78,63 @@ function moves() {
     }
 }
 
+// Game Reset or Finish Check
+function gameProcessCheck() {
+    $('.popup').html(popup);
+    // var r = confirm("게임을 다시 시작하시겠습니까? 당신의 시간은" + timerPrint);
+    // if (r == true) {
+    //     window.location.reload();
+    // } else {
+    //     alert('게임이 끝났습니다');
+    // }
+}
+
 $(document).ready(function () {
+
     let clickhold = [];
     $('.card').click(function () {
-        // Call moves Function to count move and stars.
-        
+
+        if (!started) {
+            started = true;
+            timeCount = 0;
+            timerPrint = setTimeout(startTimer, 1000);
+            return;
+        }
+
+
         // Push the card to compare each other
         clickhold.push($(this).children('.fa').attr('class'));
         console.log(clickhold);
 
         // Card Open
         $(this).addClass("open show");
+        $(this).addClass('disable');
         if (clickhold.length == 2) {
+            // Call moves Function to count move and stars.
             moves();
+            $('.card').addClass('disable');
             if (clickhold[0] === clickhold[1]) {
                 $('.open.show').removeClass("open show").addClass("match");
                 console.log('matched');
+
+                // Count matchCount to Finish the game.
+                matchCount += 1;
+
                 clickhold = [];
+                $('.card').removeClass('disable');
+
             } else {
                 console.log('not matched');
                 clickhold = [];
                 let ele = $('.card');
                 setTimeout(function () {
                     ele.removeClass("open show");
-                }, 2000);
+                    $('.card').removeClass('disable');
+                }, 1000);
+
+            }
+            if (matchCount === 1) {
+                gameProcessCheck();
             }
         }
     })
